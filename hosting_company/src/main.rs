1e2874aca57555ed179c8e5e297d541ea7750e6e
Dovydas <dovydas@yoshke.org>
2023-06-29 16:36:17 +0800

    improve

--- hosting_company/src/main.rs
+++ hosting_company/src/main.rs
@@ -1,3 +1,4 @@
+use axum::extract::State;
 use axum::response::{Html, IntoResponse};
 use axum::{
     extract::Path,
@@ -7,22 +8,26 @@ use axum::{
     Router, TypedHeader,
 };
 use html_to_string_macro::html;
+use rsa::pkcs8::LineEnding;
+use rsa::{pkcs8::EncodePublicKey, RsaPrivateKey, RsaPublicKey};
 use std::net::SocketAddr;
 
-struct Customer<'a> {
-    email: &'a str,
+#[derive(Clone)]
+struct Customer {
+    email: String,
     // In practise, we would NEVER store unhashed passwords!
-    password: &'a str,
+    password: String,
 }
 
-struct Podcast<'a> {
-    title: &'a str,
-    slug: &'a str,
-    owner: Customer<'a>,
+#[derive(Clone)]
+struct Podcast {
+    title: String,
+    slug: String,
+    owner: Customer,
 }
 
-impl Podcast<'_> {
-    fn feed(&self) -> String {
+impl Podcast {
+    fn feed(&self, public_key: RsaPublicKey) -> String {
         format!(
             "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
 <rss version=\"2.0\" xmlns:podcast=\"https://podcastindex.org/namespace/1.0\">
@@ -30,42 +35,70 @@ impl Podcast<'_> {
     <title>{}</title>
     <podcast:verify
       verifyUrl=\"http://localhost:8081/verify/{}\"
-      publicKey=\"\"
+      publicKey=\"{}\"
       />
   </channel>
 </rss>",
-            self.title, self.slug,
+            self.title,
+            self.slug,
+            pem_to_base64(public_key.to_public_key_pem(LineEnding::LF).unwrap()),
         )
     }
 }
 
-const CUSTOMER_ALICE: Customer = Customer {
-    email: "alice@example.com",
-    password: "password123",
-};
-const PODCAST_ALICE: Podcast = Podcast {
-    title: "Alice's Podcast",
-    slug: "alice-podcast",
-    owner: CUSTOMER_ALICE,
-};
+/// Removes the header and footer from a PEM-encoded key, as well as any line breaks.
+fn pem_to_base64(pem_string: String) -> String {
+    pem_string
+        .lines()
+        .filter(|line| !line.starts_with("-----"))
+        .collect::<Vec<_>>()
+        .join("")
+}
 
-const CUSTOMER_BOB: Customer = Customer {
-    email: "bob@example.com",
-    password: "password456",
-};
-const PODCAST_BOB: Podcast = Podcast {
-    title: "Bob's Podcast",
-    slug: "bob-podcast",
-    owner: CUSTOMER_BOB,
-};
-
-static PODCASTS: &[&Podcast] = &[&PODCAST_ALICE, &PODCAST_BOB];
+#[derive(Clone)]
+struct AppState {
+    podcasts: Vec<Podcast>,
+    public_key: RsaPublicKey,
+    private_key: RsaPrivateKey,
+}
 
 #[tokio::main]
 async fn main() {
+    let mut rng = rand::thread_rng();
+    let bits = 2048;
+    let private_key = RsaPrivateKey::new(&mut rng, bits).expect("failed to generate a key");
+    let public_key = RsaPublicKey::from(&private_key);
+
+    let customer_alice = Customer {
+        email: String::from("alice@example.com"),
+        password: String::from("password123"),
+    };
+    let customer_bob = Customer {
+        email: String::from("bob@example.com"),
+        password: String::from("password456"),
+    };
+
+    let podcasts = vec![
+        Podcast {
+            title: String::from("Alice's Podcast"),
+            slug: String::from("alice-podcast"),
+            owner: customer_alice,
+        },
+        Podcast {
+            title: String::from("Bob's Podcast"),
+            slug: String::from("bob-podcast"),
+            owner: customer_bob,
+        },
+    ];
+
     let router = Router::new()
         .route("/", get(root))
-        .route("/feed/:slug", get(feed));
+        .route("/feed/:slug", get(feed))
+        .with_state(AppState {
+            podcasts,
+            public_key,
+            private_key,
+        });
 
     let port = 8081;
     let addr = SocketAddr::from(([127, 0, 0, 1], port));
@@ -77,28 +110,29 @@ async fn main() {
 }
 
 async fn feed(
+    State(state): State<AppState>,
     Path(slug): Path<String>,
 ) -> Result<(TypedHeader<ContentType>, impl IntoResponse), StatusCode> {
-    let podcast = slug_to_podcast(&slug).ok_or(StatusCode::NOT_FOUND)?;
+    let podcast = slug_to_podcast(state.podcasts, &slug).ok_or(StatusCode::NOT_FOUND)?;
 
-    Ok((TypedHeader(ContentType::xml()), podcast.feed()))
+    Ok((
+        TypedHeader(ContentType::xml()),
+        podcast.feed(state.public_key),
+    ))
 }
 
-fn slug_to_podcast(slug: &str) -> Option<&Podcast> {
-    PODCASTS
-        .iter()
-        .find(|podcast| podcast.slug == slug)
-        .copied()
+fn slug_to_podcast(podcasts: Vec<Podcast>, slug: &str) -> Option<Podcast> {
+    podcasts.into_iter().find(|podcast| podcast.slug == slug)
 }
 
-async fn root() -> impl IntoResponse {
+async fn root(State(state): State<AppState>) -> impl IntoResponse {
     Html(html! {
         <h1>"Hosting Company"</h1>
         <p>"Podcasts we host:"</p>
         <ul>
         {
             let mut my_html = vec![];
-            for podcast in PODCASTS {
+            for podcast in state.podcasts {
                 my_html.push(html! {
                     <li>
                         <a
